341ca9061e44ada9d7374d25d7f0aab5acf4596f
Amune Israel <iharmuneh@gmail.com>
2024-07-23 00:51:00 +0100

    response with 200

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,15 @@
 import * as net from "net";
+import { buffer } from "stream/consumers";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
+  
+  console.log(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`))
+  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+  socket.end();
   });
-});
 
 server.listen(4221, "localhost");
