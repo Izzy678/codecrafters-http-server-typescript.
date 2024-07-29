05eb230bb80f9ea849621a93e700b22ee50ed914
Amune Israel <iharmuneh@gmail.com>
2024-07-29 07:41:34 +0100

    feat: extract url path

--- app/main.ts
+++ app/main.ts
@@ -6,9 +6,22 @@ console.log("Logs from your program will appear here!");
 
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
- // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-  socket.write(`HTTP/1.1 200 OK\r\n\r\n`)
-  socket.end();
+  socket.on("data", (data) => {
+    const path = data.toString().split(" ")[1];
+    let response = "";
+    console.log("Socket", path);
+    if (path == "/") {
+      response = `HTTP/1.1 200 OK\r\n\r\n`;
+    } else {
+      response = `HTTP/1.1 404 Not Found\r\n\r\n`;
+    }
+    socket.write(response);
+    socket.end();
   });
+  // console.log("socket",socket)
+  // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+  // socket.write(`HTTP/1.1 200 OK\r\n\r\n`)
+  // socket.end();
+});
 
 server.listen(4221, "localhost");
