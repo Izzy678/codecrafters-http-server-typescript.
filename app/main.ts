5a5979ddb32a77eb69903aaf88ee0d77bcce02c1
Amune Israel <iharmuneh@gmail.com>
2024-07-29 09:17:32 +0100

    refactor: respond with body

--- app/main.ts
+++ app/main.ts
@@ -7,9 +7,12 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const path = data.toString().split(" ")[1];
-    let response = "";
-    if (path == "/echo/abc") {
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc`;
+    const randomStringPath = path.split('/')[2];
+  
+    let response='';
+   
+    if (path == `/echo/${randomStringPath}`) {
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomStringPath.length}\r\n\r\n${randomStringPath}`;
     }
     if (path == "/") {
       response = `HTTP/1.1 200 OK\r\n\r\n`;
@@ -17,7 +20,7 @@ const server = net.createServer((socket) => {
     if (path == "/abcdefg") {
       response = `HTTP/1.1 404 Not Found\r\n\r\n`;
     }
-
+    
     socket.write(response);
     socket.end();
   });
