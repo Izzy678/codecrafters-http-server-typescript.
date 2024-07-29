3013e95a622211d3fbe36fe7ceba9151ee1a6529
Amune Israel <iharmuneh@gmail.com>
2024-07-29 08:23:38 +0100

    feat: respond with body

--- app/main.ts
+++ app/main.ts
@@ -3,18 +3,21 @@ import { buffer } from "stream/consumers";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const path = data.toString().split(" ")[1];
     let response = "";
-    console.log("Socket", path);
+    if (path == "/echo/abc") {
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc`;
+    }
     if (path == "/") {
       response = `HTTP/1.1 200 OK\r\n\r\n`;
-    } else {
+    }
+    if (path == "/abcdefg") {
       response = `HTTP/1.1 404 Not Found\r\n\r\n`;
     }
+
     socket.write(response);
     socket.end();
   });
