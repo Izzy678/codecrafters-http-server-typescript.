15067a85e2e4db05e2c90929b3b59c3fb86c4b6b
Amune Israel <iharmuneh@gmail.com>
2024-07-31 06:53:11 +0100

    new changes

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log("data",data)
+    console.log("data",data.toString())
     const requestString = data.toString();
     const requestLines = requestString.split('\r\n');
     const requestLine = requestLines[0];
