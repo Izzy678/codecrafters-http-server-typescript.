ac6278cbfc32954e122b5a163db80d86b26a63e4
Amune Israel <iharmuneh@gmail.com>
2024-07-31 06:40:49 +0100

    read header

--- app/main.ts
+++ app/main.ts
@@ -5,35 +5,29 @@ console.log("Logs from your program will appear here!");
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
+    const requestString = data.toString();
+    const requestLines = requestString.split('\r\n');
+    const requestLine = requestLines[0];
     const path = data.toString().split(" ")[1];
+    const userAgent = requestLines[3].split(": ")[1]
+  
     const randomStringPath = path.split("/")[2];
     let response = "";
-    console.log("path",path)
-    console.log("random",randomStringPath)
-    // if (path ==`${path}`) {
-    //   const newRandomStringPath = path.split('/')[1];
-    //   console.log("this should run")
-    //   response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${newRandomStringPath.length}\r\n\r\n${newRandomStringPath}`;
-    // }
     if (path ==`/echo/${randomStringPath}`) {
-      console.log("this shuld work")
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomStringPath.length}\r\n\r\n${randomStringPath}`;
+    }
+    if (path ==`/user-agent`) {
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
     }
     if (path == "/") {
       response = `HTTP/1.1 200 OK\r\n\r\n`;
     }
     if(response=="") {
-      console.log('test')
       response = `HTTP/1.1 404 Not Found\r\n\r\n`;
     }
-
     socket.write(response);
     socket.end();
   });
-  // console.log("socket",socket)
-  // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-  // socket.write(`HTTP/1.1 200 OK\r\n\r\n`)
-  // socket.end();
 });
 
 server.listen(4221, "localhost");
